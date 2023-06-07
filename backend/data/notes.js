cf8d35004aab5805ad99098c9b98572f849dbbda
Muhammad Junaid <muhammad.junaid@attech-ltd.com>
2023-06-07 11:42:55 +0500

    Initialized our Backend and Frontend

---
+++ backend/data/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+    { 
+        _id:"1",
+        title:"Day one of college",
+        content:"i made a few friends and introduced myself to a lot of new teachers",
+        category:"College"
+    },
+    { 
+        _id:"2",
+        title:"Learning some node js",
+        content:"i made a few friends and introduced myself to a lot of new teachers",
+        category:"Learning"
+    },
+    { 
+        _id:"3",
+        title:"Watch some anime",
+        content:"i made a few friends and introduced myself to a lot of new teachers",
+        category:"Entertainment"
+    },
+    { 
+        _id:"4",
+        title:"Started React js",
+        content:"i made a few friends and introduced myself to a lot of new teachers",
+        category:"Learning"
+    },
+];
+
+module.exports = notes;  
